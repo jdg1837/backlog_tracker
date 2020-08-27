efb8b9f312efd83358d2dea8b7916e9f17ad5f84
Juan Diego Gonzalez <juan.digo97@gmail.com>
2020-08-27 14:41:25 -0500

    working row dropdowns

--- read_records.php
+++ read_records.php
@@ -6,20 +6,21 @@ header('Set-Cookie: cross-site-cookie=name; SameSite=None; Secure');
     $table = 'back_log';
 
     $primaryKey = 'id';
-
+    $button = "echo \"<button>Click!</button>\"";
     $columns = array(
-        array( 'db' => 'id', 'dt' => 0 ),
-        array( 'db' => 'requestor_id',  'dt' => 1 ),
-        array( 'db' => 'tool_name',   'dt' => 2 ),
-        array( 'db' => 'type',     'dt' => 3 ),
-        array( 'db' => 'description', 'dt' => 4 ),
-        array( 'db' => 'priority',  'dt' => 5 ),
-        array( 'db' => 'tester',   'dt' => 6 ),
-        array( 'db' => 'date_filed', 'dt' => 7 ),
-        array( 'db' => 'date_closed',  'dt' => 8 ),
-        array( 'db' => 'fix_confirm',   'dt' => 9 ),
-        array( 'db' => 'image_name',     'dt' => 10 ),  
-        array( 'db' => 'status',     'dt' => 11 ),        
+        array( '',     'dt' => 0 ),
+        array( 'db' => 'id', 'dt' => 1 ),
+        array( 'db' => 'requestor_id',  'dt' => 2 ),
+        array( 'db' => 'tool_name',   'dt' => 3 ),
+        array( 'db' => 'type',     'dt' => 4 ),
+        array( 'db' => 'description', 'dt' => 5 ),
+        array( 'db' => 'priority',  'dt' => 6 ),
+        array( 'db' => 'tester',   'dt' => 7 ),
+        array( 'db' => 'date_filed', 'dt' => 8 ),
+        array( 'db' => 'date_closed',  'dt' => 9 ),
+        array( 'db' => 'fix_confirm',   'dt' => 10 ),
+        array( 'db' => 'image_name',     'dt' => 11 ),  
+        array( 'db' => 'status',     'dt' => 12 )
     );
 
     $sql_details = array(
--- webapp.js
+++ webapp.js
@@ -1,6 +1,31 @@
 $(document).ready(function() {
+    // We load the table whenever the page is loaded
     read_db();
+    
+    // Reads from db and fills table. Adds options to update and delete
+    function read_db(){
+        $('#trackertable').DataTable( {
+            "processing": true,
+            "bDestroy": true,
+            "ajax": {
+                "url": "read_records.php",
+                "type": "POST"
+            },
+            "columnDefs": [ {
+                "targets": 0,
+                "data": null,
+                "defaultContent": `<div class="dropdown">
+                <button class="btn btn-primary btn-xs dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">...</button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" id = editlink" href="#">Edit</a>
+                <a class="dropdown-item" id = "deletelink" href="#">Delete</a>
+                <div>
+              </div>`
+            } ]
+        } );
+    }
 
+    // Supercedes form submittal to send it to server with AJAX
     $("#addNewForm").submit(function(e) {
 
         e.preventDefault();
@@ -17,15 +42,20 @@ $(document).ready(function() {
             }
         });
       });
-} );
 
-function read_db(){
-    $('#tracker-table').DataTable( {
-        "processing": true,
-        "bDestroy": true,
-        "ajax": {
-            "url": "read_records.php",
-            "type": "POST"
-        }
-    } );
-}
+    //   $('#trackertable tbody').on( 'click', 'button', function () {
+    //     //alert(555);
+    //     var table = $('#trackertable').DataTable();
+    //     var data = table.row( $(this).parents('tr') ).data();
+        
+    // } );
+
+    // Action upon chosing to delete a row
+    $("#trackertable").on('click', '#deletelink', function(e){
+        e.stopPropagation();
+        e.stopImmediatePropagation();
+        var table = $('#trackertable').DataTable();
+        var data = table.row( $(this).parents('tr') ).data();
+        alert(data[1]);
+    });
+} );
